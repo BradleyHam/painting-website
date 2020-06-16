f1e511e4a5003d508f3bef77aa7126f201d48086
bradleyham <bradleyjamesham@gmail.com>
2020-06-16 17:33:58 +1200

    Updates

---
+++ precache-manifest.40c15915f30088bc62956aad2b0209cc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "415c9508d24c92b2f4380a46bf6ee987",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cf410ddab1a804512d4",
+    "url": "/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "2cf67f70e601303f0593",
+    "url": "/static/css/main.f84f4b35.chunk.css"
+  },
+  {
+    "revision": "2cf410ddab1a804512d4",
+    "url": "/static/js/2.04017c05.chunk.js"
+  },
+  {
+    "revision": "5eec01646827a9963adb60a058d89e41",
+    "url": "/static/js/2.04017c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf67f70e601303f0593",
+    "url": "/static/js/main.a5f4fb54.chunk.js"
+  },
+  {
+    "revision": "2fefcd10a5e6ff11520e",
+    "url": "/static/js/runtime-main.51241ff4.js"
+  },
+  {
+    "revision": "fab323c99a05f003322a7bc627c689db",
+    "url": "/static/media/five.fab323c9.jpg"
+  },
+  {
+    "revision": "104b14b50d6dcc1612288e0cb932f3cc",
+    "url": "/static/media/houseThree.104b14b5.jpg"
+  },
+  {
+    "revision": "f161e738f3c1963b055bb8260101cd76",
+    "url": "/static/media/houseTwo.f161e738.jpg"
+  },
+  {
+    "revision": "46183f49896e8b164dbf454e2307b10a",
+    "url": "/static/media/painter-2751665_1280.46183f49.jpg"
+  }
+]);
